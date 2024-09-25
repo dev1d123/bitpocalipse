1a842be902cee54fffce9b4a5d0788954ed76fbe
dev1d123 <dhuamanio@unsa.edu.pe>
2024-09-24 21:21:14 -0500

    Added new comments to make some actions

--- tetris/Game.cpp
+++ tetris/Game.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <thread>
+
 class Tetromino {
 	protected:
 		std::vector<std::vector<int>> shape;
-		//Corner position
 		int posX, posY;
 	public:
 		Tetromino() : posX(0), posY(0) {}
@@ -15,14 +16,23 @@ class Tetromino {
 			posX = x;
 			posY = y;
 		}
-
 };
+
 class Board{
     private:
 		static const int WIDTH = 10;
 		static const int HEIGHT = 20;
 		char arr[HEIGHT][WIDTH];
+		//Pieza actual
 	public:
+		
+	bool state(){
+		return false;
+	}
+	void newPiece(){
+		//agregar una nueva pieza al tablero!!!
+	}
+
 	void message(){
 	    std::cout<<"This work!!!"<<std::endl;
 		std::cout<<"WIDTH: "<<WIDTH<<"\tHEIGHT: "<<HEIGHT<<std::endl;	
@@ -30,23 +40,27 @@ class Board{
 	//Draws the board with all Pieces...ya know
 	
 	void draw(){
+		
 		for(int i = 0; i < HEIGHT; i++){
 			for(int j = 0; j < WIDTH; j++){
 				arr[i][j] = '.';
 			}
 		}
+		
+		/*
 		arr[4][5] = '*';
 		arr[4][6] = '*';
 		arr[4][7] = '*';
 		arr[4][8] = '*';
-		
-
+		*/
+		//dibujar la pieza con el arreglo
 		for(int i = 0; i < HEIGHT; i++){
 			for(int j = 0; j < WIDTH; j++){
 				std::cout<<arr[i][j]<<" ";
 			}
 			std::cout<<'\n';
 		}
+		//disminuir la altura de la pieza
 	}
 
 };
@@ -57,8 +71,22 @@ private:
     Board b;
 public:
     void run(){
-		b.message();
-		b.draw();
+		for(int i = 0; i < 10; i++){
+			system("cls");
+			std::cout<<"Iteracion: "<<i<<std::endl;
+			b.message();
+			//si la pieza ha llegado al final.
+			//Verificar si se ha completado un espacio
+			//generar una nueva pieza
+			
+			if(b.state()){
+				b.newPiece();
+			}
+			
+			b.draw();	
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		}
+		
     }
 };
 int main(){
